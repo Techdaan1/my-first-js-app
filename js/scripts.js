2f41687e19fec9167894c6fcfc940c45a92e3496
Techdaan1 <88482201+Techdaan1@users.noreply.github.com>
2021-08-25 12:04:15 +0200

    added comment

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,18 @@
+//List of different Pokémon and their specifications
 let pokemonList = [
-  {name: "Bulbasaur", height: 0.7, type: ['grass', 'poison']}
-  {name: "Charmander", height: 0.6, type: "fire"}
-  {name:"Squirtle", height: 0.5, type: "water"}
+  {
+    name: "Bulbasaur",
+    height: 0.7,
+    type: ['grass', 'poison']
+  },
+  {
+    name: "Charmander",
+    height: 0.6,
+    type: "fire"
+  },
+  {
+    name:"Squirtle",
+    height: 0.5,
+    type: "water"
+  }
 ]
