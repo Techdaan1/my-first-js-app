9d94b418c4ed00047ba655afc7e472cab4938248
Techdaan1 <88482201+Techdaan1@users.noreply.github.com>
2021-08-25 11:54:36 +0200

    added pokemonList

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,5 @@
-alert('Hello world');
-
-let favoriteFood = 'pasta';
-document.write(favoriteFood);
+let pokemonList = [
+  {name: "Bulbasaur", height: 0.7, type: ['grass', 'poison']}
+  {name: "Charmander", height: 0.6, type: "fire"}
+  {name:"Squirtle", height: 0.5, type: "water"}
+]
