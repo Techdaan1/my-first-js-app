a084fa3d3eb38c807ebfac5cd58c3d6104d1074d
Techdaan1 <88482201+Techdaan1@users.noreply.github.com>
2021-09-03 13:46:40 +0200

    added event listener

--- js/scripts.js
+++ js/scripts.js
@@ -46,6 +46,9 @@ function addListItem(pokemon){
   let pokemonList = document.querySelector('.pokemon-list');
   let pokemonListItem = document.createElement('li');
   let button = document.createElement('button');
+  button.addEventListener('click', function() {
+    showDetails(pokemon);
+  });
   button.innerText = pokemon.name;
   button.classList.add('button-class');
   pokemonListItem.appendChild(button);
