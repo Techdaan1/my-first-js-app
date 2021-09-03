6b773ced1b297653918b28b24524ea7efd726938
Techdaan1 <88482201+Techdaan1@users.noreply.github.com>
2021-09-03 13:16:11 +0200

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,6 @@
 //List of different Pokémon and their specifications, wrapped in an IIFE
 let pokemonRepository = (function() {
-  let pokemonList = [
+  let repository = [
   {
     name: "Bulbasaur",
     height: 0.7,
@@ -20,39 +20,50 @@ let pokemonRepository = (function() {
     name:"Butterfree",
     height: 1.1,
     type: ["bug", "flying"]
-  }
+  },
 ];
-
-// returns Pokémon list
-function getAll() {
-    return pokemonList;
-  }
 
 // Checks if the Pokémon is an object
 function add(pokemon) {
-  if (typeof pokemon === "object")
-      pokemonList.push(pokemon);
+  if (
+    typeof pokemon === "object" &&
+    "name" in pokemon &&
+    "height" in pokemon &&
+    "types" in pokemon
+  )  {
+    repository.push(pokemon);
+  } else {
+  console.log("pokemon is not correct")
+  }
 }
 
-return {
-  getAll: getAll,
-  add: add,
-  addListItem: addListItem
-};
-})();
+// returns Pokémon list
+function getAll() {
+    return repository;
+}
 
-function addListItem(pokemon) {
-  let pokemonUnList = document.querySelector('.pokemon-list');
+function addListItem(pokemon){
+  let pokemonList = document.querySelector('.pokemon-list');
   let pokemonListItem = document.createElement('li');
   let button = document.createElement('button');
   button.innerText = pokemon.name;
   button.classList.add('button-class');
   pokemonListItem.appendChild(button);
-  pokemonUnList.appendChild(pokemonListItem);
+  pokemonList.appendChild(pokemonListItem);
 }
 
-//forEach Loop of the different Pokémon, including hight conditional
+return {
+  add: add,
+  getAll: getAll,
+  addListItem: addListItem,
+  };
+})();
 
-pokemonRepository.getAll().forEach(function(pokemon) {
+console.log(pokemonRepository.getAll());
+pokemonRepository.add({ name: "Pikachu", height: 0.3, types:["electric"] });
+
+pokemonRepository.getAll().forEach(function (pokemon) {
   pokemonRepository.addListItem(pokemon);
 });
+
+//forEach Loop of the different Pokémon, including hight conditional
